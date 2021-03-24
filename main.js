b48dbf7b44ced87f914361c9bc5e5b2aaf1afda5
sonia-cerasaro <sonia.cerasaro@gmail.com>
2021-03-24 15:45:16 +0100

    left slider done

--- main.js
+++ main.js
@@ -6,7 +6,16 @@ var app = new Vue(
       arrayPictures: [
         'https://i.insider.com/5995bfb021c3fcc3788b4851?width=1100&format=jpeg&auto=webp',
         'https://cdn.images.express.co.uk/img/dynamic/151/590x/secondary/space-2023247.jpg?r=1566570895206',
-        'https://scitechdaily.com/images/Billion-Solar-Mass-Black-Hole-Crop-scaled.jpg',
+        'https://www.universetoday.com/wp-content/uploads/2019/01/DyGvigfWsAIzaxj.jpg',
       ]
     },
+    methods: { 
+      prev: function() {
+        if(this.index < this.arrayPictures.length - 1){
+          this.index++;
+        } else {
+          this.index = 0;
+        }
+      }
+    }
   });
