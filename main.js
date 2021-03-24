fedee677d30e3c154a8b28da7c619ab27b9df7d0
sonia-cerasaro <sonia.cerasaro@gmail.com>
2021-03-24 20:02:32 +0100

    corretto

--- main.js
+++ main.js
@@ -10,18 +10,19 @@ var app = new Vue(
       ]
     },
     methods: {
-      prev: function() {
-        if(this.index < this.arrayPictures.length - 1){
-          this.index++;
-        } else {
-          this.index = 0;
-        }
-      },
       next: function() {
         if(this.index > 0){
           this.index--;
         } else {
           this.index = 2;
+        }
+      },
+      prev: function() {
+
+        if(this.index < this.arrayPictures.length - 1){
+          this.index++;
+        } else {
+          this.index = 0;
         }
       },
       dotClick: function(i) {
