90ca3b8e4949949e1c8b32bf0ee3acd3ef10598a
sonia-cerasaro <sonia.cerasaro@gmail.com>
2021-03-24 15:09:28 +0100

    add images

---
+++ main.js
@@ -0,0 +1,12 @@
+var app = new Vue(
+  {
+    el: '#root',
+    data: {
+      index: 0,
+      arrayPictures: [
+        'https://i.insider.com/5995bfb021c3fcc3788b4851?width=1100&format=jpeg&auto=webp',
+        'https://cdn.images.express.co.uk/img/dynamic/151/590x/secondary/space-2023247.jpg?r=1566570895206',
+        'https://scitechdaily.com/images/Billion-Solar-Mass-Black-Hole-Crop-scaled.jpg',
+      ]
+    },
+  });
